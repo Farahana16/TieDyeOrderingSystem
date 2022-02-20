b19a893899dd84bacfea67b6d856197fc3432004
Nur Farahana <95182496+Farahana16@users.noreply.github.com>
2022-02-20 23:09:23 +0800

    Update ConnectionManager.java

--- src/main/java/db/ConnectionManager.java
+++ src/main/java/db/ConnectionManager.java
@@ -10,8 +10,8 @@ import java.sql.SQLException;
  */
 public class ConnectionManager {
     static Connection con;
-    private static final String DB_DRIVER = "jdbc:mysql://localhost:3306/"; 
-    private static final String DB_CONNECTION = "TAKA";
+    private static final String DB_DRIVER = "com.mysql.jdbc.Driver"; 
+    private static final String DB_CONNECTION = "jdbc:mysql://eu-cdbr-west-02.cleardb.net/heroku_7fab5d5a1f5a6b9";
     private static final String DB_USER = "b103e963bf8806";
     private static final String DB_PASSWORD = "1a371198";
                 
